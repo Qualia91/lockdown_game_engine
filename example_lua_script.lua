9f994b5ccf7a360afa8c1ba1c35e7b23fcdb1f5a
Nickolas Wood <53903568+Qualia91@users.noreply.github.com>
2021-01-09 13:07:49 +0000

    moved around the whole lua scripting and front end to make more user friendly

--- example_lua_script.lua
+++
@@ -1,7 +0,0 @@
-local ComponentType = luajava.bindClass( "com.boc_dev.lge_model.generated.components.ComponentType" )
-
-local timer_components = registry:getComponentMap():get(ComponentType.TIMER))
-for i, v in pairs(timer_components) do
-    print(tostring(i))
-    print(tostring(v))
-end
